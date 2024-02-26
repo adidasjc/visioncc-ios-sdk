6dd92fc664f2f4dfd842af63491b3b51cb29c6b5
chao <chao.jiang@adidas.com>
2024-02-26 12:58:41 +0800

    no message

--- Package.swift
+++ Package.swift
@@ -13,17 +13,28 @@ let package = Package(
             targets: ["VisionCCiOSSDK"]),
     ],
     dependencies: [
-        .package(name: "socket.io-client-swift", url: "https://github.com/socketio/socket.io-client-swift.git", .upToNextMinor(from: "16.1.0")),
-        .package(name: "JXSegmentedView", url: "https://github.com/pujiaxin33/JXSegmentedView.git", .upToNextMinor(from: "1.2.7")),
-        .package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.7.1")),
-        .package(name: "RxSwift", url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.5.0")),
-        .package(name: "SnapKit", url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.6.0")),
-        .package(name: "realm-swift", url: "https://github.com/realm/realm-swift.git", .upToNextMajor(from: "10.47.0")),
+        .package(url: "https://github.com/socketio/socket.io-client-swift.git", .upToNextMinor(from: "16.1.0")),
+        .package(url: "https://github.com/pujiaxin33/JXSegmentedView.git", .upToNextMinor(from: "1.2.7")),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.7.1")),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.5.0")),
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.6.0")),
+        .package(url: "https://github.com/realm/realm-swift.git", .upToNextMajor(from: "10.47.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         //.binaryTarget(name: "VisionCCiOSSDK", path: "VisionCCiOSSDK.xcframework"),
+        .target(
+            name: "VisionCCLibrary",
+            dependencies: [
+                .product(name: "socket.io-client-swift", package: "socket.io-client-swift"),
+                .product(name: "JXSegmentedView", package: "JXSegmentedView"),
+                .product(name: "Alamofire", package: "Alamofire"),
+                .product(name: "SnapKit", package: "SnapKit"),
+                .product(name: "RxSwift", package: "RxSwift"),
+                .product(name: "RealmSwift", package: "RealmSwift")
+            ]
+        ),
         .binaryTarget(
             name: "VisionCCiOSSDK", 
             url: "https://vcc-sdk.vxish.cn/VisionCCiOSSDK.xcframework.zip",
